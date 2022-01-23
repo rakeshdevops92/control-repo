049937722b352f42a2850c95fec3243cde5f69ec
Rakesh Chanamala <rakemtl@gmail.com>
2022-01-23 12:27:15 +0530

    updated file path

--- site-modules/profile/manifests/psscript.pp
+++ site-modules/profile/manifests/psscript.pp
@@ -1,6 +1,6 @@
 class profile::psscript {
   exec { 'rename-guest':
-    command   => file('test.ps1'),
+    command   => file('./test.ps1'),
     provider  => powershell,
     logoutput => true,
   }
