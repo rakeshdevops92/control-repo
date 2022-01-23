c2db3788705ad9a6e61b25f35a41a8a1cd0d35ea
Rakesh Chanamala <rakemtl@gmail.com>
2022-01-23 12:59:59 +0530

    updated path

--- site-modules/profile/manifests/psscript.pp
+++ site-modules/profile/manifests/psscript.pp
@@ -1,6 +1,6 @@
 class profile::psscript {
   exec { 'rename-guest':
-    command   => file('/etc/puppetlabs/code/environments/staging/site-modules/profile/files/test.ps1'),
+    command   => file('puppet:///site-modules/profile/files/test.ps1'),
     provider  => powershell,
     logoutput => true,
   }
