dea91933bf073b6dc937d026ea1e40c4b5c35e17
Rakesh Chanamala <rakemtl@gmail.com>
2022-01-26 01:09:02 +0530

    updated for pranitgh

--- site-modules/profile/manifests/psscript.pp
+++ site-modules/profile/manifests/psscript.pp
@@ -20,8 +20,8 @@ class profile::psscript {
     command => '
     Set-ExecutionPolicy -scope Process Bypass
     try {
-        mkdir C:/temp/test
-        invoke-webrequest -uri "https://bootdiags7y4srqsh763ko.blob.core.windows.net/test/AzureRole.ps1?sp=r&st=2022-01-25T19:13:04Z&se=2022-01-30T03:13:04Z&spr=https&sv=2020-08-04&sr=b&sig=owZBBQ6lwzEKKe8wRkvO0dktCUot4Xb%2FmEh5UH10Mpw%3D" -OutFile C:\temp\test\test.ps1
+        mkdir C:/temp/crowdstrike
+        invoke-webrequest -uri "https://bootdiags7y4srqsh763ko.blob.core.windows.net/test/AzureRole.ps1?sp=r&st=2022-01-25T19:13:04Z&se=2022-01-30T03:13:04Z&spr=https&sv=2020-08-04&sr=b&sig=owZBBQ6lwzEKKe8wRkvO0dktCUot4Xb%2FmEh5UH10Mpw%3D" -OutFile C:\temp\crowdstrike\crowdstrike.ps1
         Exit 0
     }
     catch {
