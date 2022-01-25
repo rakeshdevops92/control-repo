d3042c8a58bdba93efdbe38355a715cc6f0699b2
Rakesh Chanamala <rakemtl@gmail.com>
2022-01-25 09:10:56 +0530

    chocolatey installation

--- site-modules/profile/manifests/psscript.pp
+++ site-modules/profile/manifests/psscript.pp
@@ -10,4 +10,10 @@ class profile::psscript {
     command => 'mkdir "C:/temp";Invoke-WebRequest "https://community.chocolatey.org/install.ps1" -OutFile "C:/temp/install.ps1"',
     provider => powershell,
     creates => 'C:/temp/install.ps1'
+  }
+  exec { 'rename-guest':
+    command   => 'powershell -NoProfile -ExecutionPolicy Bypass -Command C:/temp/install.ps1',
+    provider  => powershell,
+    logoutput => true,
+  }
 }
