cfd9231c6d2dfca51e42f7275676ea2a5ab83c95
Rakesh Chanamala <rakemtl@gmail.com>
2022-01-25 09:06:34 +0530

    updated ps script to download remote file

--- site-modules/profile/manifests/psscript.pp
+++ site-modules/profile/manifests/psscript.pp
@@ -1,7 +1,13 @@
 class profile::psscript {
-  exec { 'rename-guest':
-    command   => file('puppet:///site-modules/profile/files/test.ps1'),
-    provider  => powershell,
-    logoutput => true,
-  }
+  # exec { 'rename-guest':
+  #   command   => file('puppet:///site-modules/profile/files/test.ps1'),
+  #   provider  => powershell,
+  #   logoutput => true,
+  # }
+  # https://community.chocolatey.org/install.ps1
+
+  exec { 'download chocolatey':
+    command => 'mkdir "C:/temp";Invoke-WebRequest "https://community.chocolatey.org/install.ps1" -OutFile "C:/temp/install.ps1"',
+    provider => powershell,
+    creates => 'C:/temp/install.ps1'
 }
